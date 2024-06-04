2e34612514736f2d81d555be97233406c2de23ca
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-06-04 16:09:20 +0200

    add: login and register forms routes

--- taxi-fleet-solutions-interface/src/App.jsx
+++ taxi-fleet-solutions-interface/src/App.jsx
@@ -1,22 +1,30 @@
+// Modules
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// Styles
 import "./Components/styles/stylesheet.css";
+// Packages
 import Dashboard from "./Components/Dashboard";
 import NotFound from "./Components/NotFound";
 import Sidebar from "./Components/Sidebar";
-import RegisterForm from "./Components/RegisterForm";
 import CustomerForm from "./Components/CustomerForm";
 import DriverForm from "./Components/DriverForm";
 import VehicleForm from "./Components/VehicleForm";
 import ExpenseForm from "./Components/ExpenseForm";
 import IncomeForm from "./Components/IncomeForm";
 import SalaryForm from "./Components/SalaryForm";
+import Home from "./Components/Home";
+import RegisterForm from "./Components/LoginRegisterForm/RegisterForm";
+import LoginForm from "./Components/LoginRegisterForm/LoginForm";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/salary" element={<SalaryForm />} />
         <Route path="/income" element={<IncomeForm />} />
         <Route path="/expense" element={<ExpenseForm />} />
