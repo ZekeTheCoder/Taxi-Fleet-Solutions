4bade1147a4af585e02afe5d43cd31515cc7b110
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-06-04 16:16:59 +0200

    add: login and register forms

---
+++ taxi-fleet-solutions-interface/src/Components/LoginRegisterForm/LoginForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RegisterForm from "./RegisterForm";
+
+function LoginForm() {
+  return (
+    <>
+      <h1>LoginForm</h1>
+      <RegisterForm />
+    </>
+  );
+}
+
+export default LoginForm;
---
+++ taxi-fleet-solutions-interface/src/Components/LoginRegisterForm/LoginRegisterForm.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
+
+function LoginRegisterForm() {
+  return (
+    <>
+      <h1>LoginRegisterForm</h1>
+      <LoginForm />
+      <RegisterForm />
+    </>
+  );
+}
+
+export default LoginRegisterForm;
