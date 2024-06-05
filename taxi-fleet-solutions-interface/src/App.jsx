d26ba727b34d05bed1809011c6432d9a5eb53f11
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-06-05 22:41:20 +0200

    uncommited changes

--- taxi-fleet-solutions-interface/src/App.jsx
+++ taxi-fleet-solutions-interface/src/App.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // Styles
-import "./Components/styles/stylesheet.css";
+// import "./Components/styles/stylesheet.css";
 // Packages
 import Homepage from "./Components/Homepage";
 import Dashboard from "./Components/Dashboard";
@@ -16,6 +16,8 @@ import VehicleForm from "./Components/Forms/VehicleForm";
 import ExpenseForm from "./Components/Forms/ExpenseForm";
 import IncomeForm from "./Components/Forms/IncomeForm";
 import SalaryForm from "./Components/Forms/SalaryForm";
+import Dashboard1 from "./Components/Dashboard1";
+import IncomeForm2 from "./Components/Forms/IncomeForm2";
 
 function App() {
   return (
@@ -23,12 +25,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard1" element={<Dashboard1 />} />
 
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
 
         <Route path="/salary" element={<SalaryForm />} />
         <Route path="/income" element={<IncomeForm />} />
+        <Route path="/income2" element={<IncomeForm2 />} />
         <Route path="/expense" element={<ExpenseForm />} />
         <Route path="/driver" element={<DriverForm />} />
         <Route path="/customer" element={<CustomerForm />} />
