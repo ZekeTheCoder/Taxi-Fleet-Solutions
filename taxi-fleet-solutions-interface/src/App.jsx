6ee13daf4e964a3f58b405c9235b4b8e2337a19e
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-06-05 09:31:15 +0200

    update: components path updates

--- taxi-fleet-solutions-interface/src/App.jsx
+++ taxi-fleet-solutions-interface/src/App.jsx
@@ -4,27 +4,29 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // Styles
 import "./Components/styles/stylesheet.css";
 // Packages
+import Homepage from "./Components/Homepage";
 import Dashboard from "./Components/Dashboard";
-import NotFound from "./Components/NotFound";
-import Sidebar from "./Components/Sidebar";
-import CustomerForm from "./Components/CustomerForm";
-import DriverForm from "./Components/DriverForm";
-import VehicleForm from "./Components/VehicleForm";
-import ExpenseForm from "./Components/ExpenseForm";
-import IncomeForm from "./Components/IncomeForm";
-import SalaryForm from "./Components/SalaryForm";
-import Home from "./Components/Home";
 import RegisterForm from "./Components/LoginRegisterForm/RegisterForm";
 import LoginForm from "./Components/LoginRegisterForm/LoginForm";
+import NotFound from "./Components/NotFound";
+
+import CustomerForm from "./Components/Forms/CustomerForm";
+import DriverForm from "./Components/Forms/DriverForm";
+import VehicleForm from "./Components/Forms/VehicleForm";
+import ExpenseForm from "./Components/Forms/ExpenseForm";
+import IncomeForm from "./Components/Forms/IncomeForm";
+import SalaryForm from "./Components/Forms/SalaryForm";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+
         <Route path="/salary" element={<SalaryForm />} />
         <Route path="/income" element={<IncomeForm />} />
         <Route path="/expense" element={<ExpenseForm />} />
