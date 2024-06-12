fdfb0b83799b1fc2596983d94b178353ca8bb0a6
Thabang Maphangisane <Thabangh647@gmail.com>
2024-06-12 17:40:29 +0200

    deploy: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/Taxi-Fleet-Solutions/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./src/main.jsx",
+      },
+    },
+  },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   //   base: "/Taxi-Fleet-Solutions/",
+// });
